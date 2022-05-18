4530dfb70531848bbc38a99498a755e3433dd71f
Packiaseelan <packiaseelan14@gmail.com>
2022-05-18 17:44:02 +0530

    Delete build directory
    build folder deleted

--- build/app/intermediates/flutter/profile/gen_snapshot.d
+++
@@ -1,1 +0,0 @@
-gen_snapshot.d: /Users/packiaseelan/flutter/bin/cache/artifacts/engine/android-arm-profile/darwin-x64/gen_snapshot
--- build/gen_snapshot.d
+++
@@ -1,1 +0,0 @@
-gen_snapshot.d: /Users/packiaseelan/flutter/bin/cache/artifacts/engine/android-arm-dynamic-release/darwin-x64/gen_snapshot
